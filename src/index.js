4cd4c2556877371098bbd81e39ad1456d1a90552
julia <s98435@yandex.ru>
2022-01-27 12:23:56 +0300

    feat: write solve

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    // разбиваю на массив по 10
+    const arrCode = [];
+    let sum = '';
+    for (let i = 0; i < expr.length; i++) {
+        sum += expr[i]
+        if (sum.length == 10) {
+            arrCode.push(sum)
+            sum = '';
+        }
+    }
+
+    //перебираю полученный массив
+    arrCode.map(arr => arr.split(''))
+           .forEach(arr => {
+               let itemTable = '';
+               if (arr.join('') !== '**********') {
+                    for (let i = 0; i < arr.length; i += 2) {
+                        let code = arr[i] + arr[i + 1]
+                        if (code == '10') itemTable += '.';
+                        if (code == '11') itemTable += '-';
+                    }
+                    result += MORSE_TABLE[itemTable]
+               } else {
+                   result += ' '
+               }
+           })
+    return result;
 }
 
 module.exports = {
